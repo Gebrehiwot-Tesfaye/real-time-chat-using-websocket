cfb1f054aca1819043f14530a8423e38868126f9
gebby123 <tefayegebrehiwot123@gmail.com>
2024-04-05 20:22:50 +0300

    adding postgress

--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -81,13 +81,29 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD':'admin',
+       'HOST': 'localhost',
+       'PORT': ''
+   }
+}
+#   Server [localhost]:
+# Database [postgres]:
+# Port [5432]:
+# Username [postgres]:
+# Password for user postgres:
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
