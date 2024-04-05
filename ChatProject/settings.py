7d450e631f2f4cf3ddb9ad6fddc4c09acbfad6b5
gebby123 <tefayegebrehiwot123@gmail.com>
2024-04-06 01:15:35 +0300

    first commit

--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=m1^$ql(%@l&m_d()&st30d!b^hc5cjl5@9!+*mlheo)v&o(y='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG','    False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','*').split(',') 
 
 
 # Application definition
@@ -99,11 +100,10 @@ DATABASES = {
        'PORT': '5432'
    }
 }
-#   Server [localhost]:
-# Database [postgres]:
-# Port [5432]:
-# Username [postgres]:
-# Password for user postgres:
+
+database_url =os.environ.get('DATABASE_URL')
+DATABASES["default"] = dj_database_url.parse(database_url) 
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
