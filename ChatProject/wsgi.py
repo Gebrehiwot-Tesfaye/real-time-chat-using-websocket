394ffe8658f375f3c09dc96b523510b52fd70be1
gebby123 <tefayegebrehiwot123@gmail.com>
2024-04-06 11:16:58 +0300

    some message modifed

--- ChatProject/wsgi.py
+++ ChatProject/wsgi.py
@@ -7,10 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+"""
+ASGI config for ChatProject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
 import os
 
 from django.core.wsgi import get_wsgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_wsgi_application
+from ChatApp import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatProject.settings')
 
-application = get_wsgi_application()
+django_wsgi_app = get_wsgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_wsgi_app,
+    "websocket": URLRouter(
+        routing.websocket_urlpatterns
+    )
+}) 
